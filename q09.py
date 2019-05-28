adc48645b0eb0a0fbb3ded1dd3b3355785dc9b11
Esteban Quijano Zapata <50256225+jequijanz@users.noreply.github.com>
2019-05-28 00:55:35 -0500

    Update q09.py

--- q09.py
+++ q09.py
@@ -3,5 +3,13 @@
 ## separada por ':' de los valores de la columna _c2
 ## para el archivo tbl0.tsv
 ## 
+tblaux = tbl0.groupby('_c1')['_c2'].apply(list)
 
+df = pd.DataFrame()
+df['_c1'] = tblaux.keys()
+df['_c2'] = [elem for elem in tblaux]
+
+df['_c2'] = [":".join(str(v) for v in sorted(elem)) for elem in df['_c2']]
+
+df
 
