9c5d001b4a5e4ba17677bdc2d4f3deaef6afc301
Esteban Quijano Zapata <50256225+jequijanz@users.noreply.github.com>
2019-05-28 01:01:53 -0500

    Update q11.py

--- q11.py
+++ q11.py
@@ -3,4 +3,11 @@
 ## separada por ',' de los valores de la columna _c5a
 ## y _c5b (unidos por ':') de la tabla tbl2.tsv
 ## 
-
+tbl2.head()
+tbl2['_c5'] = tbl2['_c5a'] + ":" + tbl2['_c5b'].astype('str')
+tblaux = tbl2.groupby('_c0')['_c5'].apply(list)
+df = pd.DataFrame()
+df['_c0'] = tblaux.keys()
+df['lista'] = [elem for elem in tblaux]
+df['lista'] = [",".join(str(v) for v in sorted(elem)) for elem in df['lista']]
+df
