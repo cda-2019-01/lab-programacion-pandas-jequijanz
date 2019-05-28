9055217771ce9540eadf73283357be5a8066a648
Esteban Quijano Zapata <50256225+jequijanz@users.noreply.github.com>
2019-05-28 00:59:33 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -3,4 +3,10 @@
 ## separada por ',' de los valores de la columna _c4
 ## de la tabla tbl1.tsv
 ## 
-
+tbl1.head()
+tblaux = tbl1.groupby('_c0')['_c4'].apply(list)
+df = pd.DataFrame()
+df['_c0'] = tblaux.keys()
+df['lista'] = [elem for elem in tblaux]
+df['lista'] = [",".join(str(v) for v in sorted(elem)) for elem in df['lista']]
+df
